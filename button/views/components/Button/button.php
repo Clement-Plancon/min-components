61b6680e141b6faa926f40237e422634deb6ce1d
Clement-Plancon <plancon.clem@gmail.com>
2022-11-06 18:33:20 +0100

    start repo min components

---
+++ button/views/components/Button/button.php
@@ -0,0 +1,8 @@
+<button>
+    <img src="./icon_home.svg" alt="icone bouton accueil">
+    <p>Accueil</p>
+</button>
+<button>
+    <img src="./icon_logout.svg" alt="icone bouton accueil">
+    <p>Déconnexion</p>
+</button>
